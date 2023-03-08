1b731ecfe62c48510d25fc042964a106678ff402
jeff buenting <jeffbuenting@gmail.com>
2023-03-08 13:43:38 -0500

    back to full script

--- main.old.py
+++ main.old.py
@@ -4,17 +4,7 @@
 
 
 
-        client= mqtt.Client("admin")
-        client.on_publish = on_publish
-        client.connect(broker,port)
         
-        with open(inputfile, newline='') as csvfile:
-            inputdata = csv.DictReader(csvfile)
-            for row in inputdata:
-                print(row['Url'], row['Path'])
-                #publish message
-                ret= client.publish("/data",row)
-    else:
         print("no input file specified.")
         I = 0
         while None == None:
--- main.py
+++ main.py
@@ -26,14 +26,22 @@ if __name__ == "__main__":
         port=args.port
 
         print( "Normal Processing")
-
-
-
-
-
-print("no input file specified.")
-I = 0
-while None == None:
-    print( "No Input: {}".format(I))
-    I += 1
-    time.sleep(30)
+        
+        client= mqtt.Client("admin")
+        client.on_publish = on_publish
+        client.connect(broker,port)
+        
+        with open(inputfile, newline='') as csvfile:
+            inputdata = csv.DictReader(csvfile)
+            for row in inputdata:
+                print(row['Url'], row['Path'])
+                #publish message
+                ret= client.publish("/data",row)
+                
+    else:
+        print("no input file specified.")
+        I = 0
+        while None == None:
+            print( "No Input: {}".format(I))
+            I += 1
+            time.sleep(30)
