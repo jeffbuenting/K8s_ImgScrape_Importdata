b7201072423494d0ac77bd7e01ab7fecc8f9731a
jeff buenting <jeffbuenting@gmail.com>
2023-03-19 18:32:53 -0400

    added if

--- main.py
+++ main.py
@@ -17,4 +17,30 @@ if __name__ == "__main__":
 
     print("num args = ")
     print(args)
-    print( "hello world args" )
+    print( "hello world if" )
+
+    if args.inputfile != None:
+        inputfile = args.inputfile
+        # broker=args.broker
+        # port=args.port
+
+        print( "Reading input file and passing to MQTT...")
+        
+        # client= mqtt.Client("admin")
+        # client.on_publish = on_publish
+        # client.connect(broker,port)
+        
+        # with open(inputfile, newline='') as csvfile:
+        #     inputdata = csv.DictReader(csvfile)
+        #     for row in inputdata:
+        #         print(row['Url'], row['Path'])
+                #publish message
+                # ret= client.publish("/data",row)
+
+    else:
+        raise Exception("Missing required input")
+        # I = 0
+        # while None == None:
+        #     print( "No Input: {}".format(I))
+        #     I += 1
+        #     time.sleep(30)
