e8c2ba5316d11621f92bb63ea01b526fc8c8e069
jeff buenting <jeffbuenting@gmail.com>
2023-03-26 04:37:45 -0400

    added connection loop

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import paho.mqtt.client as mqtt
 import argparse, sys
 import time
         
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        client.connected_flag=True #set flag
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=",rc)
+        client.bad_connection_flag=True
+
 def on_publish(client,userdata,result):
     print("Device 1 : Data published.")
     pass
@@ -27,12 +35,23 @@ if __name__ == "__main__":
         if args.broker != None and args.port != None:
             broker=args.broker
 
+            mqtt.Client.bad_connection_flag=False
+
             # and convert string to int as docker env vars must be string
             port=int(args.port)
 
             client= mqtt.Client()
             client.on_publish = on_publish
             client.connect(broker,port)
+
+             #
+            while not client.connected_flag and not client.bad_connection_flag: #wait in loop
+                print("In wait loop")
+                time.sleep(1)
+                
+            if client.bad_connection_flag:
+                client.loop_stop()    #Stop loop
+                sys.exit()
 
         else:
             print("MQTT broker or port is missing. writing file contents only.")
